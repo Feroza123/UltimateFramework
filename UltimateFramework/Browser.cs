9d92009be159c93e00287c640f98f85137ef9be3
Feroz Ally <feroz.ally@derivco.co.za>
2017-04-20 15:06:51 +0200

    Added Browser class

---
+++ UltimateFramework/Browser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UltimateFramework
+{
+    public static class Browser
+    {
+        private static IWebDriver _webDriver = new ChromeDriver();
+        private static string _baseUrl = "http://www.qtptutorial.net";
+
+        public static ISearchContext Driver
+        {
+            get { return _webDriver; }
+        }
+
+        public static string Title { get { return _webDriver.Title; } }
+
+        public static void Goto(string url)
+        {
+            _webDriver.Navigate().GoToUrl(_baseUrl + url);
+        }
+
+        public static void Initialize()
+        {
+
+        }
+    }
+}
